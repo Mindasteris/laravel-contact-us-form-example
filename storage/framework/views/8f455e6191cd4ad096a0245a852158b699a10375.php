f5dc51259837164f6dcbeae4dd87c1956dc6dca8
Mindaugas Šunokas <46446902+Mindasteris@users.noreply.github.com>
2022-10-26 10:42:31 +0300

    Add files via upload

---
+++ storage/framework/views/8f455e6191cd4ad096a0245a852158b699a10375.php
@@ -0,0 +1,11 @@
+
+<h2>Hello</h2> <br><br>
+You have got an email from: <?php echo e($name); ?> <br><br>
+User details: <br><br>
+Name: <?php echo e($name); ?> <br>
+Email: <?php echo e($email); ?> <br>
+Phone: <?php echo e($phone); ?> <br>
+Subject: <?php echo e($subject); ?> <br>
+Message: <?php echo e($user_query); ?> <br><br>
+Thanks,
+Have a nice day!<?php /**PATH C:\Users\msunokas\Documents\Programavimas\Laravel\contact_form\resources\views/mail_template.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/a936c2d54c6bdc4834fd6eccb299f9e6ce4a0801.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    
+    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('css/style.css')); ?>">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <!-- Pranešimas sėkmingai išsiuntus formą -->
+        <?php if(Session::has('success')): ?>
+            <div class="alert alert-success">
+                <?php echo e(Session::get('success')); ?>
+
+            </div>
+        <?php endif; ?>
+        <form method="post" action="<?php echo e(route('contact.store')); ?>">
+
+             
+             
+
+            <?php echo csrf_field(); ?>
+            <div class="form-group py-2">
+                
+                <?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="error"><?php echo e($message); ?></div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+
+                <label><i class="fa-solid fa-user"></i>&nbsp; Name</label>
+                <input type="text" class="form-control" name="name">
+            </div>
+            <div class="form-group py-2">
+                
+                <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="error"><?php echo e($message); ?></div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+
+                <label><i class="fa-solid fa-envelope"></i>&nbsp; Email</label>
+                <input type="email" class="form-control" name="email">
+            </div>
+            <div class="form-group py-2">
+                 
+                <?php $__errorArgs = ['phone'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="error"><?php echo e($message); ?></div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+
+                <label><i class="fa-sharp fa-solid fa-phone"></i>&nbsp; Phone</label>
+                <input type="text" class="form-control" name="phone">
+            </div>
+            <div class="form-group py-2">
+                 
+                <?php $__errorArgs = ['subject'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="error"><?php echo e($message); ?></div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+
+                <label><i class="fa-solid fa-check"></i>&nbsp; Subject</label>
+                <input type="text" class="form-control" name="subject">
+            </div>
+            <div class="form-group py-2">
+                 
+                <?php $__errorArgs = ['message'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="error"><?php echo e($message); ?></div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+
+                <label><i class="fa-solid fa-message"></i>&nbsp; Message</label>
+                <textarea class="form-control" name="message" rows="4"></textarea>
+            
+            </div> <br>
+            <input type="submit" name="send" value="Submit" class="btn btn-dark btn-block">
+        </form>
+    </div>
+</body>
+</html><?php /**PATH C:\Users\msunokas\Documents\Programavimas\Laravel\contact_form\resources\views/contact.blade.php ENDPATH**/ ?>
